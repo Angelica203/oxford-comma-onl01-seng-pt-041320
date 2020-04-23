47b42655348792e58c519510ac0fa1be9470022b
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-23 20:07:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,7 +13,7 @@ require "pry"
      return converted_string = array.join(' and ')  
    else
      binding.pry 
-     array[-1].insert(0, "and ")
+     array[-1].insert(0,"and ")
       # converted_string = array[0..(array_including-2)].join (', ')  
       # converted_string = converted_string + (', and ') + array[-1]  
    end
