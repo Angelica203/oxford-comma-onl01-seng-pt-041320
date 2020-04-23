d2df3ca12fe274eb40b3b509f4130e8e2a2bded7
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-23 20:16:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,10 +12,11 @@ require "pry"
    elsif array.length == 2
      return converted_string = array.join(' and ')  
    else
+     #adds "and" to last position in array
+     array[-1].insert(0,"and ") 
      
-     array[-1].insert(0,"and ") #adds "and" to last position in array
+     #adds ","and converts it to string ""
      return array.join(", ")
-      
    end
 end
 
